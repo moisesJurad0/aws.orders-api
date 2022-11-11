08c877d29184da76ff736cac12ced630947198b0
moisesJurad0 <moises003@gmail.com>
2022-11-11 14:55:16 -0500

    create order with db

--- orders_api/create.py
+++ orders_api/create.py
@@ -1,8 +1,17 @@
 import json
+import os
+
+import boto3
 
 
 def lambda_handler(event, context):
     order = json.loads(event['body'])
+    dynamodb = boto3.resource('dynamodb')
+    table_name = os.environ.get('ORDERS_TABLE')
+    table = dynamodb.Table(table_name)
+    response = table.put_item(TableName=table_name, Item=order)
+    print('response->')
+    print(response)
     return {
         'statusCode': 201,
         'headers': {},
